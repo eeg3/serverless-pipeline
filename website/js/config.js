3623b8b674c0db51824cd3154df862baab2db784
Earl Gay <earl@eeg3.net>
2017-07-29 19:47:15 -0400

    Config

--- website/js/config.js
+++ website/js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://m85syelip8.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://q8zqv6hbk1.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
