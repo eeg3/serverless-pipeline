846151e6af8d1f91eb699580ff01aae103d4d1f8
Earl Gay <earl@eeg3.net>
2017-07-29 16:43:51 -0400

    Config

--- website/js/config.js
+++ website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_M5yPmWj7s', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6avb6bpidtmj2cbhv8on2j0o2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_cCeW2cFRm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1dtokch5tq284bmq847ujibvup', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://qxsfeig3s3.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://q8zqv6hbk1.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
