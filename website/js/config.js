cca0fde0295577c632c3765cb3ad1b30b5b9ec09
Earl Gay <earl@eeg3.net>
2017-07-30 03:29:31 -0400

    Back to SAM

--- website/js/config.js
+++ website/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8EQhNr2sp', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'es57cs4f7noc44jgkdc1g66sf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_cCeW2cFRm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1dtokch5tq284bmq847ujibvup', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://fnumxmx9ga.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://q8zqv6hbk1.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
